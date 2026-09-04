8386bc5c2938004a84194a7015c41ad6d887a270
FENG <739269986@qq.com>
2026-09-04 11:35:10 +0800

    f

--- example/windows/runner/main.cpp
+++ example/windows/runner/main.cpp
@@ -10,12 +10,12 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
   //start cef deamon processes. MUST CALL FIRST
   int exit_code = initCEFProcesses(instance);
-  if (exit_code >= 0) {
-    return exit_code;
-  }
+  // if (exit_code >= 0) {
+  //   return exit_code;
+  // }
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
-  if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
+    if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
     CreateAndAttachConsole();
   }
 
@@ -29,7 +29,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
       GetCommandLineArguments();
 
   project.set_dart_entrypoint_arguments(std::move(command_line_arguments));
-
+   
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
